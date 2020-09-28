7294fd2a0708b6038e4b4d3e4a1e32802dd27f13
chiaracozzarini <chiaracozzarini@users.noreply.github.com>
2020-09-28 17:56:19 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,12 +1,53 @@
-function preload(){
-  // put preload code here
-}
+var points = Array();
 
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(windowWidth,windowHeight); // creates the canvas with dimensions of the browser dimensions
   // put setup code here
+  background("MidnightBlue"); // canvas color
+  angleMode(DEGREES);
 }
 
-function draw() {
-  // put drawing code here
+
+function mousePressed() {
+    var pointCoord = Array(mouseX, mouseY);
+    points.push(pointCoord);
+  }
+
+function draw(){
+
+  background("MidnightBlue");
+
+
+  //TEXT
+  push()
+  textAlign(CENTER);
+  stroke(lerpColor(color('snow'),color('MidnightBlue'),frameCount/350));
+  textSize(30);
+  fill(lerpColor(color('snow'),color('MidnightBlue'),frameCount/350));
+  text("Cry: click to free your tears :'(", 0, 37, width);
+  pop()
+
+  push()
+
+  strokeWeight(1.5);
+  stroke(lerpColor(color('snow'),color('MidnightBlue'),frameCount/700));
+  noFill();
+
+  t = frameCount - 50;
+
+  r = abs(sin(t*2));
+
+  for (j=0; j<points.length; j++){
+
+    //DROP
+    circle(points[j][0], points[j][1], r*10);
+
+    //WAVES
+    for(i=0; i<100; i++){
+      if(t>90*i){
+      circle(points[j][0], points[j][1], (t-90*i))
+      }
+    }
+  }
+  pop()
 }
